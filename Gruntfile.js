25b67f6f752b193ca3d7eaaebef6c09200a9b949
Rich Leland <rich.leland@messagesystems.com>
2014-08-27 15:46:39 -0400

    add default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,4 +39,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-traceur-latest');
+
+  grunt.registerTask('default', ['traceur', 'concat', 'watch']);
 };
